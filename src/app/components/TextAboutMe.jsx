1dcaae3cd23e80ac4963bb05e99bd08bd966e203
Jongabee <104025249+Jongabee@users.noreply.github.com>
2024-07-23 14:33:17 -0400

    Actualizo cv

--- src/app/components/TextAboutMe.jsx
+++ src/app/components/TextAboutMe.jsx
@@ -1,17 +1,18 @@
-
-
 const TextAboutMe = () => {
-    return (
-        <p>
-            Después de 11 largos años trabajando en la Armada Argentina, en el área de logística,
-            decidí iniciar en el mundo TI. Actualmente soy desarrollador web full stack con pasión por crear
-            aplicaciones web interactivas y responsivas. Tengo experiencia
-            trabajando con JavaScript, React, Redux, Node.js, Express, PostgreSQL,
-            HTML, CSS y Git. Aprendo rápido y siempre estoy
-            buscando ampliar mis conocimientos y habilidades. Soy un jugador de equipo y
-            estoy emocionado de trabajar con otros para crear aplicaciones increíbles.
-        </p>
-    )
-}
+  return (
+    <p>
+      Después de 9 largos años trabajando en la Armada Argentina, en el área de
+      logística, decidí iniciar en el mundo TI, el cual me aventuré hasta fines
+      de 2023 en en el Departamento de Sistemas de Información de la Armada
+      Argentina. Allí fué donde adquirí e inicié mis primeros pasos con el
+      código. Ahora soy desarrollador web full stack con pasión por crear
+      aplicaciones web interactivas y responsivas. Tengo experiencia trabajando
+      con JavaScript, React, Redux, Node.js, Express, PostgreSQL, HTML, Git.
+      Aprendo rápido y siempre estoy buscando ampliar mis conocimientos y
+      habilidades. Soy un jugador de equipo y estoy emocionado de trabajar con
+      otros para crear aplicaciones increíbles.
+    </p>
+  );
+};
 
 export default TextAboutMe;
